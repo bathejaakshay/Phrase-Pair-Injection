5684d5348bf9d3dae20ef4a80801b04421c7ed1c
abatheja4 <abatheja4@gmail.com>
2023-03-31 13:09:08 +0530

    add corpus cleanning script

--- smt.sh
+++ smt.sh
@@ -16,17 +16,19 @@ mkdir -p $corpus/$src/$tgt
 # path to the SRILM bin directory
 LM=$4
 
-$LM/i686-m64/ngram-count -order 5 -interpolate -kndiscount -unk -text $corpus/train.$tgt -lm $corpus/lang_models/$langmod.lm
-echo "lm done"
-
 # path to mosesdecoder
 moses=$5
-cd $moses/scripts/training
-echo "changed directory"
+$moses/scripts/training/clean-corpus-n.perl $corpus/train hi mr $corpus/train.clean 1 80
+
+$LM/i686-m64/ngram-count -order 5 -interpolate -kndiscount -unk -text $corpus/train.clean.$tgt -lm $corpus/lang_models/$langmod.lm
+echo "lm done"
+
+#cd $moses/scripts/training
+#echo "changed directory"
 
 # path to the external-bin-dir that contains GIZA++, mkcls, snt2cooc.out files.
 giza=$6
-./train-model.perl -root-dir $corpus/$src/$tgt/trained_model -external-bin-dir $6 -alignment grow-diag-final-and -reordering msd-bidirectional-fe --corpus $corpus/train --f $src --e $tgt --lm 0:5:$corpus/lang_models/$langmod.lm:0
+$moses/scripts/training/train-model.perl -root-dir $corpus/$src/$tgt/trained_model -external-bin-dir $6 -alignment grow-diag-final-and -reordering msd-bidirectional-fe --corpus $corpus/train.clean --f $src --e $tgt --lm 0:5:$corpus/lang_models/$langmod.lm:0
 echo "model trained"
 
 
